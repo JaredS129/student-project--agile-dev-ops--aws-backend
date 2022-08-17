3ff12159587a7c756e85b92e5596394564292699
Michael Gubb <michaelgubb@developersinstitute.co.nz>
2022-08-18 11:23:50 +1200

    Remove $ since they are for strings not react

--- src/components/questionlist/Question.js
+++ src/components/questionlist/Question.js
@@ -1,10 +1,10 @@
 const Question = ({ title, summary, topic, isAnswered }) => {
   return (
     <li>
-      <h3>${title}</h3>
-      <p>${summary}</p>
-      <p>${topic}</p>
-      <p>${isAnswered ? "Solved" : "Not answered"}</p>
+      <h3>{title}</h3>
+      <p>{summary}</p>
+      <p>{topic}</p>
+      <p>{isAnswered ? "Solved" : "Not answered"}</p>
     </li>
   );
 };
