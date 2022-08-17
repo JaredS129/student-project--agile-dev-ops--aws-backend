2a3716f125332664e446c1b3a2648f7daaf89d42
Michael Gubb <michaelgubb@developersinstitute.co.nz>
2022-08-18 11:23:56 +1200

    Finish tests

--- src/components/questionlist/Question.test.js
+++ src/components/questionlist/Question.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Question from "./Question";
 
-test("renders a question", () => {
+test("renders a solved question", () => {
   render(
     <Question
       title="TestQuestion"
@@ -13,4 +13,24 @@ test("renders a question", () => {
   expect(screen.getByRole("heading", { level: 3 })).toHaveTextContent(
     "TestQuestion"
   );
+  expect(screen.getByText("TestSummary")).toBeInTheDocument();
+  expect(screen.getByText("test")).toBeInTheDocument();
+  expect(screen.getByText("Solved")).toBeInTheDocument();
+});
+
+test("renders an unsolved question", () => {
+  render(
+    <Question
+      title="TestQuestion"
+      summary="TestSummary"
+      topic="test"
+      isAnswered={false}
+    />
+  );
+  expect(screen.getByRole("heading", { level: 3 })).toHaveTextContent(
+    "TestQuestion"
+  );
+  expect(screen.getByText("TestSummary")).toBeInTheDocument();
+  expect(screen.getByText("test")).toBeInTheDocument();
+  expect(screen.getByText("Not answered")).toBeInTheDocument();
 });
