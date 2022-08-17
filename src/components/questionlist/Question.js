e1c7aa8a0db70f7fa87ce5b3fffbcab7166a2af9
Michael Gubb <michaelgubb@developersinstitute.co.nz>
2022-08-18 11:00:51 +1200

    Add data to component display, isAnswered not correct

--- src/components/questionlist/Question.js
+++ src/components/questionlist/Question.js
@@ -1,5 +1,12 @@
-const Question = () => {
-  return;
+const Question = (title, summary, topic, isAnswered) => {
+  return (
+    <li>
+      <p>${title}</p>
+      <p>${summary}</p>
+      <p>${topic}</p>
+      <p>${isAnswered}</p>
+    </li>
+  );
 };
 
 export default Question;
