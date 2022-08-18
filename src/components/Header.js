957a958394b8d29c78a730baaa0f62156bcba4b2
Campbell Moroney <campbellmoroney@gmail.com>
2022-08-18 15:41:45 +1200

    Added CSS file for Header. Spaced contents

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,22 @@
 // import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   return (
     <header>
-      <div>
-        <h1>Questionz.io</h1>
+      <div className="header">
+        <h1 className="header__title">Questionz.io</h1>
         <form>
-          <label htmlFor="title">Search for a question...</label>
+          <label htmlFor="title" className="header__search">
+            Search for a question...
+          </label>
           <input type="text" id="title" required />
         </form>
         <ul>
-          <li>
+          <li className="header__nav--link">
             <p>Log In</p>
           </li>
-          <li>
+          <li className="header__nav--link">
             <p>Dashboard</p>
           </li>
         </ul>
