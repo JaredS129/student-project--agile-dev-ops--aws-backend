94745672ee4cb58ec96728e4634be4be9cc969fd
Campbell Moroney <campbellmoroney@gmail.com>
2022-08-18 16:18:05 +1200

    Header CSS relatively matches wireframe

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,24 @@ const Header = () => {
   return (
     <header>
       <div className="header">
-        <h1 className="header__title">Questionz.io</h1>
-        <form>
-          <label htmlFor="title" className="header__search">
-            Search for a question...
-          </label>
-          <input type="text" id="title" required />
-        </form>
+        <div>
+          <h1 className="header__title">Questionz.io</h1>
+          <form>
+            <label htmlFor="title"></label>
+            <input
+              className="header__search"
+              type="text"
+              id="title"
+              required
+              value="Search for a question..."
+            />
+          </form>
+        </div>
         <ul>
           <li className="header__nav--link">
             <p>Log In</p>
           </li>
-          <li className="header__nav--link">
+          <li className="header__nav--link grey">
             <p>Dashboard</p>
           </li>
         </ul>
