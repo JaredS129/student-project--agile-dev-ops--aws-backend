1bcddef6d031dc839f396a480ca7b1cc52f3725a
JaredS129 <jaredkevin.stevenson@gmail.com>
2022-08-18 10:28:46 +0700

    add key prop to question component

--- src/components/questionlist/Question.js
+++ src/components/questionlist/Question.js
@@ -1,6 +1,6 @@
-const Question = ({ title, summary, topic, isAnswered }) => {
+const Question = ({ key, title, summary, topic, isAnswered }) => {
   return (
-    <li>
+    <li key={key}>
       <h3>{title}</h3>
       <p>{summary}</p>
       <p>{topic}</p>
