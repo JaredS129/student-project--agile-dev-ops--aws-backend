60dfa52955be22858a33f3c06da3fd80c23f02f3
Campbell Moroney <campbellmoroney@gmail.com>
2022-08-18 15:08:09 +1200

    Refactored api.js and api.test.js with json file

--- src/api.js
+++ src/api.js
@@ -1,20 +1,7 @@
-const getQuestions = async () => {
-  const testQuestions = [
-    {
-      question_id: 1,
-      title: "How to pat dogs",
-    },
-    {
-      question_id: 2,
-      title: "How to pat cats",
-    },
-    {
-      question_id: 3,
-      title: "How to juggle dogs",
-    },
-  ];
+import questions from "./data/questions.json";
 
-  return testQuestions;
+const getQuestions = async () => {
+  return questions;
 };
 
 export { getQuestions };
