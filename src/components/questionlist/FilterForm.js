ada00858c3a709d13b91bbde1a465d5ebfa0f0a2
Michael Gubb <michaelgubb@developersinstitute.co.nz>
2022-08-19 20:12:23 +1200

    Add unanswered/answered form

--- src/components/questionlist/FilterForm.js
+++ src/components/questionlist/FilterForm.js
@@ -1,5 +1,21 @@
-const FilterForm = () => {
-  return <form></form>;
+const FilterForm = ({ useAnswered, setUseAnswered }) => {
+  return (
+    <form>
+      <label htmlFor="answered">Choose unanswered/answered:</label>
+      <select
+        id="answered"
+        name="answered"
+        value={useAnswered}
+        onChange={(x) => {
+          setUseAnswered(x.target.value);
+        }}
+      >
+        <option value="unanswered">unanswered</option>
+        <option value="answered">answered</option>
+        <option value="both">both</option>
+      </select>
+    </form>
+  );
 };
 
 export default FilterForm;
--- src/components/questionlist/QuestionList.js
+++ src/components/questionlist/QuestionList.js
@@ -1,25 +1,48 @@
 import Question from "./Question";
+import FilterForm from "./FilterForm";
+import React, { useState } from "react";
 import "./QuestionList.css";
 
+const filterByAnswered = (isAnswered, useAnswered) => {
+  // if useAnswered is both then return true
+  if (useAnswered === "both") {
+    return true;
+  }
+  if (useAnswered === "answered" && isAnswered) {
+    return true;
+  }
+  if (useAnswered === "unanswered" && !isAnswered) {
+    return true;
+  }
+  return false;
+};
+
 const QuestionList = ({ questions, searchText }) => {
+  const [useAnswered, setUseAnswered] = useState("both");
   return (
-    <ul>
-      {questions
-        .filter((question) =>
-          question.title.toLowerCase().includes(searchText.toLowerCase())
-        )
-        .map((question) => (
-          <div className="question-container">
-            <Question
-              key={question.question_id}
-              title={question.title}
-              topic={question.topic}
-              summary={question.summary}
-              isAnswered={question.isAnswered}
-            />
-          </div>
-        ))}
-    </ul>
+    <div>
+      <FilterForm useAnswered={useAnswered} setUseAnswered={setUseAnswered} />
+      <ul>
+        {questions
+          .filter((question) =>
+            question.title.toLowerCase().includes(searchText.toLowerCase())
+          )
+          .filter((question) =>
+            filterByAnswered(question.isAnswered, useAnswered)
+          )
+          .map((question) => (
+            <div className="question-container">
+              <Question
+                key={question.question_id}
+                title={question.title}
+                topic={question.topic}
+                summary={question.summary}
+                isAnswered={question.isAnswered}
+              />
+            </div>
+          ))}
+      </ul>
+    </div>
   );
 };
 
