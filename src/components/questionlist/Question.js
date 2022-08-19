8a8d2f3820f575c78bd296a49aaab8930dd6951e
JaredS129 <jaredkevin.stevenson@gmail.com>
2022-08-19 09:31:35 +0700

    change div to span for 'solved' and 'Un answered' text

--- src/components/questionlist/Question.js
+++ src/components/questionlist/Question.js
@@ -5,7 +5,13 @@ const Question = ({ title, summary, topic, isAnswered }) => {
     <li>
       <div className="grid-container">
         <h3 className="title-format">{title}</h3>
-        <p className="answer-format">{isAnswered ? <div className="solved-format">Solved</div> : <div className="not-answered-format">Not answered</div>}</p>
+        <p className="answer-format">
+          {isAnswered ? (
+            <span className="solved-format">Solved</span>
+          ) : (
+            <span className="not-answered-format">Not answered</span>
+          )}
+        </p>
       </div>
       <p className="summary-format">{summary}</p>
       <p className="topic-format">{topic}</p>
