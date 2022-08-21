517aea7ef76341be44dce48bc2a0d5f9af036647
Michael Gubb <michaelgubb@developersinstitute.co.nz>
2022-08-22 10:29:01 +1200

    Add js to pathname

--- src/components/fullQuestion/FullQuestion.test.js
+++ src/components/fullQuestion/FullQuestion.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import FullQuestion from "./FullQuestion"
+import FullQuestion from "./FullQuestion.js"
 
 test("renders a full question", () => {
   render(
