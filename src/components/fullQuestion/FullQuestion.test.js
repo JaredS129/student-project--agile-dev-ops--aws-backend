59ed815cc23e1cd701a27d13ec74070a21e50488
Michael Gubb <michaelgubb@developersinstitute.co.nz>
2022-08-22 10:18:12 +1200

    Update import pathname to include js

--- src/components/fullQuestion/FullQuestion.test.js
+++ src/components/fullQuestion/FullQuestion.test.js
@@ -1,25 +1,25 @@
 import { render, screen } from "@testing-library/react";
-import FullQuestion from "./FullQuestion"
+import FullQuestion from "./FullQuestion.js";
 
 test("renders a full question", () => {
-    render(<FullQuestion
-        title="TestQuestion"
-        summary="TestSummary"
-        topic="testTopic"
-        isAnswered="isAnsweredTest"
-        workOn="workOnTest"
-        tryAchieve="tryAchieveTest"
-        issue="issueTest"
-        attempted="attemptedTest"
-        />)
-        expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
-            "TestQuestion"
-          );
-          expect(screen.getByText("testTopic")).toBeInTheDocument();
-          expect(screen.getByText("workOnTest")).toBeInTheDocument();
-          expect(screen.getByText("tryAchieveTest")).toBeInTheDocument();
-          expect(screen.getByText("issueTest")).toBeInTheDocument();
-          expect(screen.getByText("attemptedTest")).toBeInTheDocument();
-
-
-})
+  render(
+    <FullQuestion
+      title="TestQuestion"
+      summary="TestSummary"
+      topic="testTopic"
+      isAnswered="isAnsweredTest"
+      workOn="workOnTest"
+      tryAchieve="tryAchieveTest"
+      issue="issueTest"
+      attempted="attemptedTest"
+    />
+  );
+  expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+    "TestQuestion"
+  );
+  expect(screen.getByText("testTopic")).toBeInTheDocument();
+  expect(screen.getByText("workOnTest")).toBeInTheDocument();
+  expect(screen.getByText("tryAchieveTest")).toBeInTheDocument();
+  expect(screen.getByText("issueTest")).toBeInTheDocument();
+  expect(screen.getByText("attemptedTest")).toBeInTheDocument();
+});
