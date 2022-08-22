fd75bead31f23f0b6b61c1f347cd83d28f5327bb
Michael Gubb <michaelgubb@developersinstitute.co.nz>
2022-08-22 19:03:32 +1200

    Add placeholders

--- src/components/fullQuestion/FullQuestion.js
+++ src/components/fullQuestion/FullQuestion.js
@@ -6,15 +6,15 @@ const FullQuestion = (props) => {
     <div>
       <h2>{props.title ?? id}</h2>
       <h3>Topic</h3>
-      <p>{props.topic}</p>
+      <p>{props.topic ?? "placeholder"}</p>
       <h3>What have you worked on?</h3>
-      <p>{props.workOn}</p>
+      <p>{props.workOn ?? "placeholder"}</p>
       <h3>What are you trying to achieve?</h3>
-      <p>{props.tryAchieve}</p>
+      <p>{props.tryAchieve ?? "placeholder"}</p>
       <h3>What is the issue you are having?</h3>
-      <p>{props.issue}</p>
+      <p>{props.issue ?? "placeholder"}</p>
       <h3>What have you attempted?</h3>
-      <p>{props.attempted}</p>
+      <p>{props.attempted ?? "placeholder"}</p>
     </div>
   );
 };
