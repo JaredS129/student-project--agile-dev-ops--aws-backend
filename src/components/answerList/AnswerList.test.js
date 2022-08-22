12d19bfd0a298bd3c435d4ea7ec4b04719d1d312
Campbell Moroney <campbellmoroney@gmail.com>
2022-08-22 16:13:38 +1200

    Added tests for AnswerList.js (not working)

---
+++ src/components/answerList/AnswerList.test.js
@@ -0,0 +1,39 @@
+import { render, screen, within } from "@testing-library/react";
+import AnswerList from "./AnswerList";
+import answerData from "../../data/answers.json";
+
+describe("AnswerList", () => {
+  const answers = answerData["1"];
+  render(<AnswerList answers={answers} />);
+  const listItems = screen.getAllByRole("listitem");
+
+  test("renders a list of answers", () => {
+    expect(listItems).toHaveLength(answers.length);
+  });
+
+  test("renders the correct answer titles", () => {
+    listItems.forEach((item, index) => {
+      const { getByRole } = within(item);
+      const { answerTitle } = answers[index];
+      expect(getByRole("heading", { level: 3 })).toHaveTextContent(answerTitle);
+    });
+  });
+
+  test("renders the correct summaries", () => {
+    listItems.forEach((item, index) => {
+      const { getByText } = within(item);
+      const { answerText } = answers[index];
+      expect(getByText(answerText)).toHaveTextContent(answerText);
+    });
+  });
+
+  test("renders if the question is the solution", () => {
+    listItems.forEach((item, index) => {
+      const { getByText } = within(item);
+      const { goodAnswer } = answers[index];
+      expect(getByText(goodAnswer ? "Solution" : null)).toHaveTextContent(
+        goodAnswer ? "Solution" : null
+      );
+    });
+  });
+});
