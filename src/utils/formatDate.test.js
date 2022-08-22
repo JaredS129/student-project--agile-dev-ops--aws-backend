f44032de9df432bb5e1311454d6bf92137715a43
Michael Gubb <michaelgubb@developersinstitute.co.nz>
2022-08-23 11:20:35 +1200

    Update unit test to have array input

--- src/utils/formatDate.test.js
+++ src/utils/formatDate.test.js
@@ -1,7 +1,15 @@
 import { formatDate } from "./formatDate";
 
-test("Formats an ISO8601 string to Day Month Year string", () => {
-  const inputString = "blah blah blah";
-  const outputString = "bloo bloo bloo";
-  expect(formatDate(inputString)).toBe(outputString);
-});
+const testDates = [
+  {
+    inputString: "1997-07-16T19:20:30.45+01:00",
+    expectedString: "16 Jul 1997",
+  },
+];
+
+test.each(testDates)(
+  "Formats an ISO8601 string to Day Month Year string",
+  ({ inputString, expectedString }) => {
+    expect(formatDate(inputString)).toBe(expectedString);
+  }
+);
