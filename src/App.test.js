b74b5f698ff34a75546d09df8e8be3d312fc83d0
Michael Gubb <michaelgubb@developersinstitute.co.nz>
2022-08-22 17:28:05 +1200

    Add browser router to test

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,25 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 test("renders title", async () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+
   const linkElement = await screen.findByText(/Questionz.io/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test("renders lists of questions", async () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+
   const linkElement = await screen.findByRole("list");
   expect(linkElement).toBeInTheDocument();
 });
