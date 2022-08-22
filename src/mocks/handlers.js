015591e7c1677cbca2ec77cc9bd3f54b3db33d04
Michael Gubb <michaelgubb@developersinstitute.co.nz>
2022-08-22 15:29:56 +1200

    Use test environment variable to handler url

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,7 +3,7 @@ import questionList from "../data/questions.json";
 
 export const handlers = [
   // Handles a GET /questions request
-  rest.get("/questions", (req, res, ctx) => {
+  rest.get(`${process.env.REACT_APP_API_URL}/questions`, (req, res, ctx) => {
     return res(ctx.json(questionList));
   }),
   // Handles a POST /questions request
