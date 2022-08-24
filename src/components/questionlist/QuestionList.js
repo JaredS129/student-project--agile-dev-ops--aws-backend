cb02c89c96e2d26f3ce0d6d20dba99707bbaddbf
JaredS129 <jaredkevin.stevenson@gmail.com>
2022-08-24 15:18:08 +0700

    render ask question button and filter controls if there are no questions in the list

--- src/components/questionlist/QuestionList.js
+++ src/components/questionlist/QuestionList.js
@@ -1,29 +1,7 @@
 import Question from "./Question";
 import FilterForm from "./FilterForm";
-import React, { useState } from "react";
 import "./QuestionList.css";
 import { Link } from "react-router-dom";
-
-// const filterByAnswered = (isAnswered, useAnswered) => {
-//   // if useAnswered is both then return true
-//   if (useAnswered === "both") {
-//     return true;
-//   }
-//   if (useAnswered === "answered" && isAnswered) {
-//     return true;
-//   }
-//   if (useAnswered === "unanswered" && !isAnswered) {
-//     return true;
-//   }
-//   return false;
-// };
-
-// const filterByTopic = (topic, useTopic) => {
-//   if (useTopic === "All") {
-//     return true;
-//   }
-//   return topic === useTopic;
-// };
 
 const QuestionList = ({
   questions,
@@ -34,7 +12,20 @@ const QuestionList = ({
   setTopic,
 }) => {
   if (questions.length === 0) {
-    return <p>No questions yet!</p>;
+    return (
+      <div>
+        <Link className="link-format" to="/askQuestion">
+          Ask Question
+        </Link>
+        <FilterForm
+          answered={answered}
+          setAnswered={setAnswered}
+          topic={topic}
+          setTopic={setTopic}
+        />
+        <p>No questions yet!</p>
+      </div>
+    );
   }
   return (
     <div>
