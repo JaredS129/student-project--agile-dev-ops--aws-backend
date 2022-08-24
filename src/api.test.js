7536c7cdcd6a9eabe9d73ecc91a495fae3a3a9aa
JaredS129 <jaredkevin.stevenson@gmail.com>
2022-08-24 09:57:01 +0700

    refactor api tests to use msw

--- src/api.test.js
+++ src/api.test.js
@@ -1,17 +1,35 @@
+import { getQuestions } from "./api";
 import questions from "./data/questions.json";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+const server = setupServer(
+  rest.get("/api/questions", (req, res, ctx) => {
+    return res(ctx.json(questions));
+  })
+);
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
 
 describe("getQuestions", () => {
+  const expectedQuestions = questions;
+
   test("When the api call is successful, getQuestions returns data", async () => {
-    //Given
-    const expectedResponse = {
-      body: questions,
-      error: undefined,
-    };
-    const getQuestions = jest.fn();
-    getQuestions.mockReturnValueOnce(expectedResponse);
-    // When
     const response = await getQuestions();
-    //Then
-    expect(response.body).toEqual(expectedResponse.body);
+    expect(response.body).toEqual(expectedQuestions);
+  });
+
+  test("When the api call is unsuccessful, getQuestions returns error", async () => {
+    server.use(
+      rest.get("/api/questions", (req, res, ctx) => {
+        return res(ctx.status(500));
+      })
+    );
+    const response = await getQuestions();
+
+    expect(response.error).toBeTruthy();
   });
 });
