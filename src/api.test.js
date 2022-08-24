96763f6b8792f62ddce5abb0d62648838e40f8e9
JaredS129 <jaredkevin.stevenson@gmail.com>
2022-08-24 09:01:41 +0700

    adjust api.test.js to mock api response

--- src/api.test.js
+++ src/api.test.js
@@ -1,9 +1,17 @@
-import { getQuestions } from "./api";
 import questions from "./data/questions.json";
 
-test("getQuestions returns data", async () => {
-  const expectedQuestions = questions;
-  const response = await getQuestions();
-
-  expect(response.body).toEqual(expectedQuestions);
+describe("getQuestions", () => {
+  test("When the api call is successful, getQuestions returns data", async () => {
+    //Given
+    const expectedResponse = {
+      body: questions,
+      error: undefined,
+    };
+    const getQuestions = jest.fn();
+    getQuestions.mockReturnValueOnce(expectedResponse);
+    // When
+    const response = await getQuestions();
+    //Then
+    expect(response.body).toEqual(expectedResponse.body);
+  });
 });
